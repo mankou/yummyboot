8d83f256acae8c0ef47c2916b2dd3a3ebc6b6024
mang <man003@163.com>
2019-07-29 10:30:53 +0800

    * 查询表格的例子

--- src/main/resources/static/pages/hello/hello.js
+++ src/main/resources/static/pages/hello/hello.js
@@ -1,20 +1,64 @@
 $(document).ready(function(){
-   console.log('hello12');
-    var url = "/helloJson/queryUser";
-    $.ajax({
-        url: url,
-        // data: data,
-        dataType: "json",
-        contentType: 'application/json',
-        async: true,
-        type: "GET",
-        success: function (result) {
-            console.log(JSON.stringify(result));
-            var resultStr=JSON.stringify(result)
-           $('.result').text(resultStr);
+    var hello={
+        init:function(){
+            hello.queryUser();
         },
-        error: function (msg) {
-            alert("查询异常");
+        queryUser:function(){
+            var url = "/user/selectAll";
+            $.ajax({
+                url: url,
+                dataType: "json",
+                contentType: 'application/json',
+                async: true,
+                type: "GET",
+                success: function (result) {
+                    var status=result.STATUS;
+                    if(status=='0000'){
+                        var data=result.DATA;
+                        var resultStr=JSON.stringify(data);
+                        console.debug("查询结果:"+resultStr);
+                        // $('.result').text(resultStr);
+                        hello.drawTable(data);
+
+                    }else{
+                        alert("查询出错,返回错误码:")
+                    }
+
+                },
+                error: function (msg) {
+                    alert("查询异常");
+                }
+            });
+        },
+        drawTable:function(data){
+            $('#table1').bootstrapTable({
+                striped: false,                      //是否显示行间隔色
+                sortable: true,                    //是否启用排序
+                sortName:'code',
+                sortOrder: "asc",                 //排序方式
+                data: data,
+                columns: [
+                    {
+                        field: 'id',
+                        title: 'id'
+                    },
+                    {
+                        field: 'code',
+                        title: '编码'
+                    },
+                    {
+                        field: 'name',
+                        title: '姓名'
+                    },
+                    {
+                        field: 'createDate',
+                        title: '创建时间'
+                    }
+                ],
+            });
         }
-    });
-})
+    }
+
+
+    hello.init();
+});
