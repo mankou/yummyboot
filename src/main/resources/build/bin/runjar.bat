f69d38d476c83a5488dc86dc91e42e82266e0027
maning <man003@163.com>
2020-01-10 15:40:11 +0800

    * 添加windows上的启动脚本

---
+++ src/main/resources/build/bin/runjar.bat
@@ -0,0 +1,29 @@
+@echo off
+:: desc windows上启动jar包
+:: 开发的初衷是用于配合启动基于springboot的jar包
+:: create by m-ning at 2019-08-12
+:: last modify at 2019-08-12
+:: mail man003@163.com
+:: version 1.0.1-20200110
+
+
+::=================使用说明===========================
+:: 在env.cmd 中配置相关信息 然后双击该脚本运行
+
+
+::==================main bat==================================
+echo 正在运行,请稍后....
+echo batPath:%~dp0
+
+::将当前路径切换到脚本所在路径 当使用相对路径时会用到 如在其它地方或者计划任务中运行脚本有可能找不到路径
+cd %~dp0%\..
+
+echo load env.cmd ...
+call config\env.cmd
+
+::start program
+echo java  %JAVA_OPTS% -jar lib\%jarName% %PROGRAM_CONFIG%
+java  %JAVA_OPTS% -jar lib\%jarName% %PROGRAM_CONFIG%
+@pause
+
+
---
+++ src/main/resources/build/config/dev/env.cmd
@@ -0,0 +1,12 @@
+:: windows上启动脚本runjar.bat的配置文件
+:: 设置jar包名
+set jarName=yummyboot-0.0.1-SNAPSHOT.jar
+
+::设置虚拟机参数 
+::set JAVA_OPTS=-Xms2g -Xmx2g -XX:PermSize=128M -XX:MaxPermSize=128M -Xmn128M -XX:+UseParNewGC -XX:+UseConcMarkSweepGC
+::如果springboot打包时采用jar包分离的方式 则使用-Dloader.path=lib
+set JAVA_OPTS=-Xms256m -Xmx512m -Dloader.path=lib
+
+::设置一些程序参数
+set PROGRAM_CONFIG=--spring.profiles.active=dev
+
