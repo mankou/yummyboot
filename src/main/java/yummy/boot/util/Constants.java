422b73ac5e5908c44c84662f5a7c6542cae59fc8
maning <man003@163.com>
2021-03-31 17:41:24 +0800

    feature 常量 增加一个常量类

---
+++ src/main/java/yummy/boot/util/Constants.java
@@ -0,0 +1,11 @@
+package yummy.boot.util;
+
+public class Constants {
+
+    public static class  FlowInstanceStatus{
+        public static final String ON_GOING="进行中";
+        public static final String COMPLETED="已完成";
+        public static final String TO_BE_STARTED="待启动";
+    }
+
+}
